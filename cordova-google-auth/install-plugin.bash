193b5c563955ad5db06a7680d1b0950fdd5b1951
Tobias Mesquita <tobias.mesquita@gmail.com>
2019-09-11 10:11:22 -0300

    Create install-plugin.bash

---
+++ cordova-google-auth/install-plugin.bash
@@ -0,0 +1,3 @@
+$ cd src-cordova
+$ cordova plugin add cordova-plugin-googleplus --save --variable --variable WEB_APPLICATION_CLIENT_ID=125087317410-9t2kd1cqea5fmu5d1hb2di2pc6vaoe3i.apps.googleusercontent.com
+$ cd..
